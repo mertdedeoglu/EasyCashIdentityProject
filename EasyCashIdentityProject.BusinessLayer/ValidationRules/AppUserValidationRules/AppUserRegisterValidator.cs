baee4a54c9bd9d4c2a462a7f43400f1331f2e2f1
mert dedeoglu <mert.dedeoglu.1903@hotmail.com>
2025-10-02 01:03:16 +0300

    -add Dtos Folders -add fluentValidation and ValidationRules

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                    .WithMessage("Ad alanı boş geçilemez.")
+                .MaximumLength(30)
+                    .WithMessage("Lutfen en fazla 30 karakter girisi yapin.")
+                .MinimumLength(1)
+                    .WithMessage("Lutfen en az 2 karakter veri girisi yapin.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alani bos gecilemez.");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanici alani bos gecilemez.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                    .WithMessage("Email alani bos gecilemez.")
+                .EmailAddress()
+                    .WithMessage("Lutfen gecerli bir mail adresi giriniz.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Sifre alani bos gecilemez.");
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty()
+                    .WithMessage("Sifre tekrar alani bos gecilemez.")
+                .Equal(y => y.Password)
+                    .WithMessage("Parolaniz eslesmiyor.");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    //Ad,Soyad,Username,Mail,Password,ConfirmPassword
+    public class AppUserRegisterDto
+    {
+        //[Required(ErrorMessage = "Must Name")]
+        //[Display(Name = "Isim")]
+        //[MaxLength(30,ErrorMessage = "En fazla 30 karakter girebilirsiniz.")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
